a70e16f5ff376e023d30862a8ad3a09b82d488a2
qpwoeirut <stanleyzhong8@gmail.com>
2023-04-21 23:26:13 -0700

    Add type hints and make formatting better

--- twomaze/carnegie.py
+++ twomaze/carnegie.py
@@ -1,10 +1,5 @@
-"""
-Edit this file! This is the file you will submit.
-"""
-
-
 # The function called for maze pattern 1
-def carnegie_1(x, y, walls_horizontal, clock_times):
+def carnegie_1(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
     MAZE_SIZE = 32
     VIEW_SIZE = 8
 
@@ -19,18 +14,18 @@ def carnegie_1(x, y, walls_horizontal, clock_times):
 
 
 # The function called for maze pattern 2
-def carnegie_2(x, y, walls_horizontal, clock_times):
+def carnegie_2(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
     MAZE_SIZE = 32
     VIEW_SIZE = 8
 
     # Greedily move up
     steps = 0
-    while (y + steps < MAZE_SIZE - 1 and steps < 7):
-        if (walls_horizontal[VIEW_SIZE][VIEW_SIZE + steps + 1] == 1):
+    while y + steps < MAZE_SIZE - 1 and steps < 7:
+        if walls_horizontal[VIEW_SIZE][VIEW_SIZE + steps + 1] == 1:
             break
         steps = steps + 1
 
-    return (steps, 5)
+    return steps, 5
 
 
 # The function called for maze pattern 3
--- twomaze/mellon.py
+++ twomaze/mellon.py
@@ -1,34 +1,30 @@
-"""
-Edit this file! This is the file you will submit.
-"""
-
 # The function called for maze pattern 1
-def mellon_1(x, y, walls_vertical, clock_times):
+def mellon_1(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
     MAZE_SIZE = 32
     VIEW_SIZE = 8
 
     # Greedily move right
     steps = 0
-    while (x + steps < MAZE_SIZE - 1 and steps < 7):
-        if (walls_vertical[VIEW_SIZE + steps + 1][VIEW_SIZE] == 1):
+    while x + steps < MAZE_SIZE - 1 and steps < 7:
+        if walls_vertical[VIEW_SIZE + steps + 1][VIEW_SIZE] == 1:
             break
         steps = steps + 1
 
-    return (steps, 5)
+    return steps, 5
 
 # The function called for maze pattern 2
-def mellon_2(x, y, walls_vertical, clock_times):
+def mellon_2(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
     MAZE_SIZE = 32
     VIEW_SIZE = 8
 
     # Greedily move right
     steps = 0
-    while (x + steps < MAZE_SIZE - 1 and steps < 7):
-        if (walls_vertical[VIEW_SIZE + steps + 1][VIEW_SIZE] == 1):
+    while x + steps < MAZE_SIZE - 1 and steps < 7:
+        if walls_vertical[VIEW_SIZE + steps + 1][VIEW_SIZE] == 1:
             break
         steps = steps + 1
 
-    return (steps, 5)
+    return steps, 5
 
 # The function called for maze pattern 3
 def mellon_3(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
