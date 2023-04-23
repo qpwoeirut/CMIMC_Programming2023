2e2d49f072c9cc911796e212e07ca37e127ca6a6
qpwoeirut <stanleyzhong8@gmail.com>
2023-04-23 16:33:08 -0700

    Add half-baked idea that does very well on pattern 1 except for the times it gets stuck

--- twomaze/carnegie.py
+++ twomaze/carnegie.py
@@ -3,17 +3,73 @@ MELLON_MOVES = "LR"
 MAZE_SIZE = 32
 VIEW_SIZE = 8
 
+WALL_FOLLOW_SIGNAL = 9 + 5
+
+def calculate_visited(movements: list[str]) -> tuple[set[tuple[int, int]], set[tuple[int, int]]]:
+    carnegie_visited = set()
+    mellon_visited = set()
+
+    x, y = 0, 0
+    for movement in movements:
+        if movement == 'L':
+            mellon_visited.add((x, y))
+            x -= 1
+            mellon_visited.add((x, y))
+        elif movement == 'R':
+            mellon_visited.add((x, y))
+            x += 1
+            mellon_visited.add((x, y))
+        elif movement == 'D':
+            carnegie_visited.add((x, y))
+            y -= 1
+            carnegie_visited.add((x, y))
+        elif movement == 'U':
+            carnegie_visited.add((x, y))
+            y += 1
+            carnegie_visited.add((x, y))
+    return carnegie_visited, mellon_visited
+
+
+def calculate_movements(clock_times: list[int]) -> list[str]:
+    moves = [(clock - 5) - 1 for clock in clock_times[1:]]
+    raw_movements = []
+    for i, move in enumerate(moves):
+        move_str = CARNEGIE_MOVES if i % 2 == 0 else MELLON_MOVES
+        for _ in range(abs(move)):
+            if move >= 0:
+                raw_movements.append(move_str[1])
+            else:
+                raw_movements.append(move_str[0])
+    return raw_movements
+
+
 def carnegie_1(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
-    return carnegie_wall_following(x, y, walls_horizontal, clock_times)
+    max_clock = max(clock_times)
+    if max_clock >= WALL_FOLLOW_SIGNAL:
+        return carnegie_wall_following(x, y, walls_horizontal, clock_times, right_wall=max_clock & 1)
+
+    if y == MAZE_SIZE - 1:
+        return 0, WALL_FOLLOW_SIGNAL
+
+    movements = calculate_movements(clock_times)
+    carnegie_visited, _ = calculate_visited(movements)
+
+    dy = 0
+    if (x, y + 1) not in carnegie_visited:
+        dy = carnegie_greedy(y, walls_horizontal, 0)
+    if dy == 0 and carnegie_greedy(y, walls_horizontal, 1) <= -1:
+        dy = -1
+
+    return dy, (dy + 1) + 5
 
 
 def carnegie_2(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
-    return carnegie_wall_following(x, y, walls_horizontal, clock_times)
+    return carnegie_wall_following(x, y, walls_horizontal, clock_times, right_wall=0)
 
 
 # classic wall-following strategy for mazes. maintain the direction we're going in and always turn left if possible
 def carnegie_3(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
-    return carnegie_wall_following(x, y, walls_horizontal, clock_times)
+    return carnegie_wall_following(x, y, walls_horizontal, clock_times, right_wall=0)
 
 
 def carnegie_greedy(y: int, walls_horizontal: list[list[int]], current_dir: int) -> int:
@@ -39,7 +95,7 @@ def compute_dy(y: int, walls_horizontal: list[list[int]], current_dir: int) -> i
     return dy
 
 
-def carnegie_wall_following(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
+def carnegie_wall_following(x: int, y: int, walls_horizontal: list[list[int]], clock_times: list[int], right_wall: int) -> tuple[int, int]:
     if x == MAZE_SIZE - 1:  # check if we can reach the end in this next move, just in case
         dy = 0
         while dy < 7:
@@ -49,7 +105,7 @@ def carnegie_wall_following(x: int, y: int, walls_horizontal: list[list[int]], c
         if y + dy == MAZE_SIZE - 1:
             return dy, 5
 
-    if clock_times[-1] == 0:
+    if clock_times[-1] == 0 or clock_times[-1] >= WALL_FOLLOW_SIGNAL:
         current_dir = 0  # start going up
         try_both_sides = 0
     else:
@@ -59,11 +115,11 @@ def carnegie_wall_following(x: int, y: int, walls_horizontal: list[list[int]], c
         f"current_dir={current_dir}, try_both_sides={try_both_sides}, clock_times[-1]={clock_times[-1]}"
 
     # if we're at the edge of the maze, we don't need to try and turn each time
-    if x == 0 and current_dir == 0:  # up
+    if x == 0 and current_dir ^ right_wall == 0:  # up
         dy = carnegie_greedy(y, walls_horizontal, current_dir)
         return dy, (2 | (abs(dy) == 7)) + 5  # if we've hit a wall, turn right immediately
 
-    if x == MAZE_SIZE - 1 and current_dir == 1:  # down
+    if x == MAZE_SIZE - 1 and current_dir ^ right_wall == 1:  # down
         dy = carnegie_greedy(y, walls_horizontal, current_dir)
         return dy, (2 | (abs(dy) < 7)) + 5  # if we've hit a wall, turn left immediately
 
@@ -73,16 +129,17 @@ def carnegie_wall_following(x: int, y: int, walls_horizontal: list[list[int]], c
         current_dir ^= 1  # we know mellon's blocked, so we turn all the way around
         dy = compute_dy(y, walls_horizontal, current_dir)
 
-    next_dir = current_dir if dy == 0 else current_dir ^ 1
+    next_dir = (current_dir if dy == 0 else current_dir ^ 1) ^ right_wall
     blocked = (walls_horizontal[VIEW_SIZE][VIEW_SIZE + dy + 1] if current_dir == 0 else walls_horizontal[VIEW_SIZE][VIEW_SIZE + dy]) ^ 1
     return dy, ((blocked << 1) | next_dir) + 5
 
 
 def main():
     from twomaze.grader import TwoMazeGrader
-    grader = TwoMazeGrader(2, True)
-    grader.grade()
-    grader.print_result()
+    while True:
+        grader = TwoMazeGrader(1, True)
+        grader.grade()
+        grader.print_result()
 
 
 if __name__ == "__main__":
--- twomaze/mellon.py
+++ twomaze/mellon.py
@@ -2,15 +2,31 @@ from carnegie import *
 
 
 def mellon_1(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
-    return mellon_wall_following(x, y, walls_vertical, clock_times)
+    max_clock = max(clock_times)
+    if max_clock >= WALL_FOLLOW_SIGNAL:
+        return mellon_wall_following(x, y, walls_vertical, clock_times, right_wall=max_clock & 1)
+
+    if x == MAZE_SIZE - 1:
+        return 0, WALL_FOLLOW_SIGNAL + 1  # add 1 to signal right-wall following, not left-wall following
+
+    movements = calculate_movements(clock_times)
+    _, mellon_visited = calculate_visited(movements)
+
+    dx = 0
+    if (x + 1, y) not in mellon_visited:
+        dx = mellon_greedy(y, walls_vertical, 0)
+    if dx == 0 and mellon_greedy(y, walls_vertical, 1) <= -1:
+        dx = -1
+
+    return dx, (dx + 1) + 5
 
 
 def mellon_2(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
-    return mellon_wall_following(x, y, walls_vertical, clock_times)
+    return mellon_wall_following(x, y, walls_vertical, clock_times, right_wall=0)
 
 
 def mellon_3(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
-    return mellon_wall_following(x, y, walls_vertical, clock_times)
+    return mellon_wall_following(x, y, walls_vertical, clock_times, right_wall=0)
 
 
 def mellon_greedy(x: int, walls_vertical: list[list[int]], current_dir: int) -> int:
@@ -36,7 +52,7 @@ def compute_dx(x: int, walls_vertical: list[list[int]], current_dir: int) -> int
     return dx
 
 
-def mellon_wall_following(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int]) -> tuple[int, int]:
+def mellon_wall_following(x: int, y: int, walls_vertical: list[list[int]], clock_times: list[int], right_wall: int) -> tuple[int, int]:
     if y == MAZE_SIZE - 1:  # check if we can reach the end in this next move, just in case
         dx = 0
         while dx < 7:
@@ -46,7 +62,7 @@ def mellon_wall_following(x: int, y: int, walls_vertical: list[list[int]], clock
         if x + dx == MAZE_SIZE - 1:
             return dx, 5
 
-    if clock_times[-1] == 0:
+    if clock_times[-1] == 0 or clock_times[-1] >= WALL_FOLLOW_SIGNAL:
         current_dir = 0  # start going right
         try_both_sides = 0
     else:
@@ -56,11 +72,11 @@ def mellon_wall_following(x: int, y: int, walls_vertical: list[list[int]], clock
         f"current_dir={current_dir}, try_both_sides={try_both_sides}, clock_times[-1]={clock_times[-1]}"
 
     # if we're at the edge of the maze, we don't need to try and turn each time
-    if y == 0 and current_dir == 1:  # left + left wall or right + right wall
+    if y == 0 and current_dir ^ right_wall == 1:  # left + left wall or right + right wall
         dx = mellon_greedy(x, walls_vertical, current_dir)
         return dx, (2 | (abs(dx) == 7)) + 5  # if we've hit a wall, turn up immediately
 
-    if y == MAZE_SIZE - 1 and current_dir == 0:
+    if y == MAZE_SIZE - 1 and current_dir ^ right_wall == 0:
         dx = mellon_greedy(x, walls_vertical, current_dir)
         return dx, (2 | (abs(dx) < 7)) + 5  # if we've hit a wall, turn down immediately
 
@@ -70,6 +86,6 @@ def mellon_wall_following(x: int, y: int, walls_vertical: list[list[int]], clock
         current_dir ^= 1  # we know carnegie's blocked, so we turn all the way around
         dx = compute_dx(x, walls_vertical, current_dir)
 
-    next_dir = current_dir ^ 1 if dx == 0 else current_dir
+    next_dir = (current_dir ^ 1 if dx == 0 else current_dir) ^ right_wall
     blocked = (walls_vertical[VIEW_SIZE + dx + 1][VIEW_SIZE] if current_dir == 0 else walls_vertical[VIEW_SIZE + dx][VIEW_SIZE]) ^ 1
     return dx, ((blocked << 1) | next_dir) + 5
